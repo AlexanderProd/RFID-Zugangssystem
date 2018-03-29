101f329dbe6446f8196d4f35dcfb87bec34cb002
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2018-03-29 09:45:13 +0200

    adding helpful comments

--- server/post.php
+++ server/post.php
@@ -1,6 +1,7 @@
 <?php
 include("time.php");
 
+//MySQL server login credentials
 $servername = "bernd-mysql.php-friends.de";
 $username = "521_admin";
 $password = "Cb&0fv";
@@ -13,23 +14,25 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
 		die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
-echo "<br/>";
+echo "Connected successfully" . "<br/>";
 
+//Values getting send to server
 $var1=$_POST["value1"];
 $var2=$_POST["value2"];
 $var3=$_POST["value3"];
 $var4=serverTime();
-echo $var4;
 
+// Insert Values into MySQL Database
 $query = "INSERT INTO `accounts` (`id`,`firstName`, `lastName` , `checkInTime`)
 VALUES ('".$var1."','".$var2."','".$var3."','".$var4."')";
 
+// Debugging Feedback
 if (mysqli_query($conn, $query)) {
 	echo "New record created successfully" . "<br />";
 } else {
 	echo "Error: " . $query . "<br>" . mysqli_error($conn);
 }
-#mysql_query($query,$conn);
+
+//Close connection when done
 mysqli_close($conn);
 ?>
