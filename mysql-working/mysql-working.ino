721872ad27e65df25785305c8c68d5a2786a6ae5
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2018-04-01 22:35:13 +0200

    update led timer

--- mysql-working/mysql-working.ino
+++ mysql-working/mysql-working.ino
@@ -29,9 +29,9 @@ void setup() {
     //LED Feedack if WiFi Connection cant be established
     for (int i=0; i <= 5; i++){
       digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-      delay(500);                       // wait for a second
+      delay(200);                       // wait for a second
       digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-      delay(500);         
+      delay(200);         
    }
   }
 
