8f65d9be0ebf4de71c06428f8d6966d3c64761af
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2018-04-01 14:13:20 +0200

    LED feedback if WiFi isnt working

--- mysql-working/mysql-working.ino
+++ mysql-working/mysql-working.ino
@@ -25,6 +25,14 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
+
+    //LED Feedack if WiFi Connection cant be established
+    for (int i=0; i <= 5; i++){
+      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+      delay(500);                       // wait for a second
+      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+      delay(500);         
+   }
   }
 
   Serial.println("");
