cf100d54196b3fd54bb04244bd96eda29360b2ea
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2018-04-14 18:59:53 +0200

    boostrap css

--- server/index.php
+++ server/index.php
@@ -1,23 +1,33 @@
 <!doctype html>
 <html lang="de">
 <head>
-  <meta charset="utf-8">
-  <title>Angemeldete Personen</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<title>Angemeldete Personen</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/skeleton.css">
 </head>
+
 <body>
 
-<div class="container">
-<input id="input" placeholder="dd.mm.yyyy">
-<button onclick="action()">GO</button>
-</br>
+<div class="container" style="margin-top: 50px;">
+  <h4>Datum:</h4>
+  <form>
+    <div class="form-row">
+      <div class="col-sm-3 my-1">
+        <input id="input" class="form-control mb-2" placeholder="dd.mm.yyyy">
+      </div>
+      <div class="col-auto my-1">
+        <button onclick="goButton()" type="button" class="btn btn-outline-secondary">GO</button>
+      </div>
+    </div>
+  </form>
+  </br>
 
 <?php
+echo "<p>";
 include("time.php");
 
 $servername = "bernd-mysql.php-friends.de";
@@ -38,14 +48,14 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
-echo "<br/>";
+echo "<br/></p>";
 
 $sql = "SELECT id, firstName, lastName, date, time FROM logins WHERE date = '$dateTransferred'";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
-    echo '<table class="u-full-width">';
+    echo '<table class="table table-striped table-sm">';
     echo "<thead><tr><th>ID</th><th>Name</th><th>Datum</th><th>Uhrzeit</th></tr></thead>";
     while($row = mysqli_fetch_assoc($result)) {
         echo "<tbody><tr><td>";
@@ -67,8 +77,15 @@ mysqli_close($conn);
 ?>
 </div>
 
+<!-- Bootstrap JS and JQuery-->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
+integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
+integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 <script>
-function action(){
+function goButton(){
   var input = document.getElementById('input').value;
   input = encodeURIComponent(input);
   window.location.href = "index.php?date=" + input;
